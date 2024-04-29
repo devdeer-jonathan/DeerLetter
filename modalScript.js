a52220da2301d65617c9dba2c00d08328ca51a82
Jonathan Lenz <jonathan.lenz@devdeer.com>
2024-04-29 13:58:33 +0200

    minor changes

---
+++ modalScript.js
@@ -0,0 +1,10 @@
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+  modal.showModal();
+});
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
